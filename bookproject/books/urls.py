fcd59e10157153e2c3c940177afad2d77743363b
lwj0712 <dldnjswot@naver.com>
2024-08-12 12:49:30 +0900

    update_CreateView

--- bookproject/books/urls.py
+++ bookproject/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView
 
 app_name = 'books' # URL 네임스페이스를 설정하여 프로젝트 내에서 URL 이름이 겹치는 것을 방지합니다.
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'), # books/로 접속하면 BookListView가 실행되도록 설정
-    path('<int:pk>', BookDetailView.as_view(), name = 'book_detail'),
+    path('<int:pk>', BookDetailView.as_view(), name = 'book_detail'),path('new/', BookCreateView.as_view(), name='book_create'),
 ] 
--- bookproject/books/views.py
+++ bookproject/books/views.py
@@ -1,16 +1,35 @@
 from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Book
+from .forms import BookForm
 
+# Create your views here.
 class BookListView(ListView):
-    model = Book  # 사용할 모델 지정
-    template_name = 'books/book_list.html'  # 사용할 템플릿 지정
-    context_object_name = 'books'  # 템플릿에서 사용할 컨텍스트 객체 이름 지정 (옵션)
+    model = Book # 이 뷰에서 사용할 모델
+    template_name = 'books/book_list.html' # 랜더링할 템플릿 파일! 
+    context_object_name = 'books' # 컨텍스트 객체 이름! 기본적으로 object_list를 사용함 books 변경
     ordering = ['-publication_date'] # 출판일 기준 내림차순으로 정렬
+    paginate_by = 2 # 페이지네이션 기능을 사용하고 한 페이지에 2개의 객체를 보여줌
 
 class MainView(TemplateView):
     template_name = 'main.html' # 랜더링할 템플릿 파일
 
+# URL패턴을 지정!
+# Template를 구성해서 사용자가 접근할 수 있도록 함!
+# 첫번째 화면이 구성되게 됩니다!!
+
+# 컨텍스트 객체 이름 -> 원래는 object_list로 사용되지만, book_list로 변경
+
 class BookDetailView(DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
+    model = Book # 이 뷰에서 사용할 모델
+    template_name = 'books/book_detail.html' # 랜더링할 템플릿 파일
+    context_object_name = 'book' # 컨텍스트 객체 이름! 기본적으로 object를 사용함 book 변경
+    
+class BookCreateView(CreateView):
+    model = Book 
+    form_class = BookForm
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('books:book_list') # 성공시 이동할 페이지
+    # reverse_lazy -> URL 이름을 지연해서 평가 -> 뷰가 로드된 다음에 URL을 생성
+    # 뷰가 로드 되기 전에 URL이 사용되는 상황에서 유용하다. -> 안전하게 URL을 참조하는 기능
